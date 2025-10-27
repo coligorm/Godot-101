62a8b66f4b51bd45c48414b62b74037eb4569bc0
coligorm <colinrhysgorman@gmail.com>
2025-10-27 23:44:22 +1100

    Basics of GDScript

---
+++ godot-introduction/Scripts/intro_to_scripting.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var score : int = 0
+var move_speed : float = 2.5
+var game_over : bool = false
+var ability : String = "slash"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(score)
+	print(move_speed)
+	print(game_over)
+	print(ability)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
