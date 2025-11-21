d7272c43fb18da72a64dfb38c826b92c872c40ee
coligorm <colinrhysgorman@gmail.com>
2025-11-21 23:37:42 +1100

    Tweaked lvl1 & begin lvl2

--- brackeys-first-2d-game/brackeys-first-2d-game/scripts/game_manager.gd
+++ brackeys-first-2d-game/brackeys-first-2d-game/scripts/game_manager.gd
@@ -1,8 +1,20 @@
 extends Node
 
+@export var score_goal : int
+@export var total_coins : int
+
 var score = 0
 @onready var score_label: Label = $ScoreLabel
+@onready var end_label: Label = $"../Labels/EndLabel"
+@onready var tile_map: TileMap = $"../TileMap"
 
 func add_point():
 	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
+	score_label.text = "You collected " + str(score) + " out of " + str(total_coins) + " coins."
+
+func _process(delta: float) -> void:
+	if score >= score_goal:
+		end_label.text = "Congradulations!\nThe goal was : " + str(score_goal)
+		tile_map.layers
+	else:
+		end_label.text = "Uh-oh!\nThe goal is : " + str(score_goal)
