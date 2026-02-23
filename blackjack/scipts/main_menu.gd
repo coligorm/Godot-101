6c8e1e32f9dd9cdc73ce8996750ab1dbf1135c14
Colin Gorman <colinrhysgorman@gmail.com>
2026-02-23 23:35:32 +0000

    Added About Menu

---
+++ blackjack/scipts/main_menu.gd
@@ -0,0 +1,31 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_button_new_game_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_button_continue_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_button_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_button_about_pressed() -> void:
+	$About_Popup.show()
+
+func _on_button_back_pressed() -> void:
+	$About_Popup.hide()
+
+
+
+
+func _on_button_exit_pressed() -> void:
+	get_tree().quit()
