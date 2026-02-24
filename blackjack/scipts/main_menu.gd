0f66da1fdde5d7ad0eff3bdaea2564f1f4de5145
Colin Gorman <colinrhysgorman@gmail.com>
2026-02-24 00:14:30 +0000

    Added Settings Options
    done issue #2

--- blackjack/scipts/main_menu.gd
+++ blackjack/scipts/main_menu.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var fullscreen = false;
+var sound = true;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,6 +22,25 @@ func _on_button_settings_pressed() -> void:
 
 func _on_settings_back_pressed() -> void:
 	$Settings_Popup.hide()
+
+# TODO: TURN SOUNDS ON AND OFF 
+func _on_sound_button_pressed() -> void:
+	if(sound):
+		$Settings_Popup/Settings_Buttons/Sound_Settings/Sound_Button.text="OFF"
+		sound = false;
+	else:
+		$Settings_Popup/Settings_Buttons/Sound_Settings/Sound_Button.text="ON"
+		sound = true;
+
+func _on_fullscreen_button_pressed() -> void:
+	if(fullscreen):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		$Settings_Popup/Settings_Buttons/Fullscreen_Settings/Fullscreen_Button.text="ENABLE"
+		fullscreen = false;
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		$Settings_Popup/Settings_Buttons/Fullscreen_Settings/Fullscreen_Button.text="DISABLE"
+		fullscreen = true;
 
 func _on_button_about_pressed() -> void:
 	$About_Popup.show()
